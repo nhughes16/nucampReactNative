05844884784f4339909fc7004c9d27a63a33de08
nhughes16 <34460434+nhughes16@users.noreply.github.com>
2021-04-12 21:20:28 -0400

    initilized redux control of app

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,15 @@ import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
 import { createAppContainer } from 'react-navigation'
 import { Icon } from 'react-native-elements'
 import SafeAreaView from 'react-native-safe-area-view'
+import { connect } from 'react-redux'
+import { fetchCampsites, fetchComments, fetchPartners, fetchPromotions } from '../redux/ActionCreators'
+
+const mapDispatchToProps = {
+	fetchCampsites,
+	fetchComments,
+	fetchPartners,
+	fetchPromotions,
+}
 
 const DirectoryNavigator = createStackNavigator(
 	{
@@ -141,6 +150,13 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
+	componentDidMount() {
+		this.props.fetchCampsites()
+		this.props.fetchComments()
+		this.props.fetchPartners()
+		this.props.fetchPromotions()
+	}
+
 	render() {
 		return (
 			<View
@@ -184,4 +200,4 @@ const styles = StyleSheet.create({
 	},
 })
 
-export default Main
+export default connect(null, mapDispatchToProps)(Main)
